9496c53d19a2bf7031e9f1e1a452ccd074e43a61
Your Name <you@example.com>
2020-01-18 13:36:30 +0530

    Initial Commit

---
+++ DemoFirestore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoFirestore
+//
+//  Created by Zaman Meraj on 18/01/20.
+//  Copyright © 2020 Zaman Meraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
